ea0bf67918e3e763d2afd41f9693eec1391fd4a0
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-05-26 01:18:26 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['MAIL_SERVER'] = 'smtp.example.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your_email@example.com'
-app.config['MAIL_PASSWORD'] = 'your_password'
+app.config.from_object('config.Config')
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 mail = Mail(app)
+csrf = CSRFProtect(app)
 
 from app import routes
