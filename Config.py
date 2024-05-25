4e4db2447c569be57a154fb2178e2a5953d36a0c
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-05-26 01:20:31 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'your_email@gmail.com'
+    MAIL_PASSWORD = 'your_email_password'
